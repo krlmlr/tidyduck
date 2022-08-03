61df1013e1ec51380c75dd80e2d7db74911eb6a2
Kirill Müller <krlmlr+r@mailbox.org>
2022-08-03 05:09:58 +0200

    tidyduck

---
+++ R/tidyduck.R
@@ -0,0 +1,14 @@
+#' DuckDB high-level bindings for R
+#'
+#' R high-level bindings for DuckDB: an embeddable SQL OLAP Database Management System.
+#'
+#' @seealso
+#' [duckdb::duckdb()] for connection instructions.
+#'
+#' <https://duckdb.org/> for the project website.
+#'
+## usethis namespace: start
+## usethis namespace: end
+#' @docType package
+#' @name tidyduck-package
+NULL
